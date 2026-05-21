50f2e57500328884a30875661e75e7a1780e7074
Psyhopassport <nazarsadygaa@gmail.com>
2026-05-21 10:06:10 +0300

    Delete OscarUA directory

--- OscarUA/pages/new.js
+++
@@ -1,8 +0,0 @@
-import { MOVIES } from '../data/movies.js';
-import { MovieGrid, SectionTitle } from '../js/components.js';
-
-export function renderNew(container) {
-  const movies = MOVIES.filter(m => m.isNew);
-  SectionTitle('🎬 Новинки кіно', container);
-  MovieGrid(movies, container);
-}
